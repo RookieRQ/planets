764733003274765dd290e7b62a282bf5fdf9e501
Ruiqi Zhang <zhangrq1997@gmail.com>
2024-09-18 10:17:26 +0200

    Add VSCode settings for Python testing

--- simulate.py
+++ simulate.py
@@ -59,6 +59,17 @@ def calculate_forces(positions, masses):
     return calculate_forces_range(args)
 
 
+def test_force_between_planets():
+    position1 = np.array([0.0, 0.0, 0.0])
+    mass1 = 1.0
+    position2 = np.array([1.0, 0.0, 0.0])
+    mass2 = 2.0
+
+    force = force_between_planets(position1, mass1, position2, mass2)
+
+    assert np.allclose(force, [2.0, 0.0, 0.0])
+
+
 def calculate_forces_parallel(positions, masses, num_cores):
     num_planets = len(positions)
     chunk_size = num_planets // num_cores
